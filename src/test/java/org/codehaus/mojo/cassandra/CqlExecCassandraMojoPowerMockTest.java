013adae677b1c3b3b4357d46df409349081020e3
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2015-06-29 17:22:47 +0200

    add some tests unit tests

---
+++ src/test/java/org/codehaus/mojo/cassandra/CqlExecCassandraMojoPowerMockTest.java
@@ -0,0 +1,159 @@
+package org.codehaus.mojo.cassandra;
+
+import org.apache.cassandra.thrift.Cassandra;
+import org.apache.cassandra.thrift.InvalidRequestException;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+import org.codehaus.plexus.util.IOUtil;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.internal.stubbing.answers.DoesNothing;
+import org.mockito.internal.stubbing.answers.ThrowsException;
+import org.mockito.stubbing.Answer;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@PrepareForTest({FileReader.class, IOUtil.class, Utils.class})
+@PowerMockIgnore({ "org.apache.xerces.*", "javax.xml.parsers.*", "org.xml.sax.*", "org.w3c.dom.*", "javax.management.*" })
+public class CqlExecCassandraMojoPowerMockTest {
+
+    private final static String CQL_STATEMENT = "CREATE KEYSPACE identifier WITH replication = {'class': 'SimpleStrategy'}";
+
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
+
+    @Mock
+    private Log log;
+
+    @Mock
+    private Cassandra.Client client;
+
+    private CqlExecCassandraMojoBuilder builder;
+
+    @Before
+    public void createCqlExecCassandraMojoBuilder() {
+        this.builder = new CqlExecCassandraMojoBuilder(log);
+    }
+
+    @Test
+    public void should_fail_if_file_not_found_occurs_when_reading_cql_script() {
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.cqlScript(file("emptyfile.cql")).build();
+        mockToThrows(new FileNotFoundException());
+
+        try {
+            cqlExecCassandraMojo.execute();
+            fail();
+        } catch (MojoExecutionException e) {
+            assertThat(e.getMessage(), allOf(startsWith("Cql file '"), endsWith("emptyfile.cql' was deleted before I could read it")));
+        } catch (MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_fail_if_io_error_occurs_when_reading_cql_script() {
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.cqlScript(file("emptyfile.cql")).build();
+        mockToThrows(new IOException());
+
+        try {
+            cqlExecCassandraMojo.execute();
+            fail();
+        } catch (MojoExecutionException e) {
+            assertEquals("Could not parse or load cql file", e.getMessage());
+        } catch (MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_use_default_cql_version() {
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.cqlStatement(CQL_STATEMENT).build();
+        ArgumentCaptor<ThriftApiOperation> operation = mockThriftExecution();
+
+        try {
+            cqlExecCassandraMojo.execute();
+
+            assertEquals("3.4.0", operation.getValue().getCqlVersion());
+        } catch (MojoExecutionException | MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_use_custom_keyspace() {
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.keyspace("identifier").cqlStatement(CQL_STATEMENT).build();
+        ArgumentCaptor<ThriftApiOperation> operation = mockThriftExecution();
+
+        try {
+            cqlExecCassandraMojo.execute();
+
+            assertEquals("identifier", operation.getValue().getKeyspace());
+        } catch (MojoExecutionException | MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_fail_when_request_fails() {
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.cqlStatement(CQL_STATEMENT).build();
+        mockThriftExecutionWith(new ThrowsException(new ThriftApiExecutionException(new InvalidRequestException("bad statement"))));
+
+        try {
+            cqlExecCassandraMojo.execute();
+            fail();
+        } catch (MojoExecutionException e) {
+            assertEquals("There was a problem calling Apache Cassandra's Thrift API. Details: The request was not properly formatted bad statement", e.getMessage());
+        } catch (MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    private ArgumentCaptor<ThriftApiOperation> mockThriftExecution() {
+        return mockThriftExecutionWith(new DoesNothing());
+    }
+
+    private ArgumentCaptor<ThriftApiOperation> mockThriftExecutionWith(Answer<Object> answer) {
+        mockStatic(Utils.class);
+        ArgumentCaptor<ThriftApiOperation> operation = ArgumentCaptor.forClass(ThriftApiOperation.class);
+        try {
+            when(Utils.class, "executeThrift", operation.capture()).thenAnswer(answer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return operation;
+    }
+
+    private void mockToThrows(Throwable throwable) {
+        try {
+            mockStatic(IOUtil.class);
+            when(IOUtil.toString(any(FileReader.class))).thenThrow(throwable);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    private File file(String name) {
+        try {
+            return new File(getClass().getResource(name).toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/org/codehaus/mojo/cassandra/CqlExecCassandraMojoTest.java
@@ -0,0 +1,139 @@
+package org.codehaus.mojo.cassandra;
+
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.cassandra.exceptions.SyntaxException;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.logging.Level;
+
+import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.WARNING;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CqlExecCassandraMojoTest {
+
+    @Mock
+    private Log log;
+
+    private CqlExecCassandraMojoBuilder builder;
+
+    @Before
+    public void createCqlExecCassandraMojoBuilder() {
+        this.builder = new CqlExecCassandraMojoBuilder(log);
+    }
+
+    @Test
+    public void should_do_nothing_when_skip() {
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.skip().build();
+
+        try {
+            cqlExecCassandraMojo.execute();
+
+            assertLog(INFO, "Skipping cassandra: cassandra.skip==true");
+        } catch (MojoExecutionException | MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_fail_when_comparator_type_parser_class_does_not_exist() {
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.comparator("NonExistingComparator").build();
+
+        try {
+            cqlExecCassandraMojo.execute();
+        } catch (MojoExecutionException e) {
+            assertEquals("Could not parse comparator value: NonExistingComparator", e.getMessage());
+            assertEquals(ConfigurationException.class, e.getCause().getClass());
+        } catch (MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_fail_when_comparator_type_parser_leads_to_syntax_error() {
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.comparator("BytesType(`").build();
+
+        try {
+            cqlExecCassandraMojo.execute();
+        } catch (MojoExecutionException e) {
+            assertEquals("Could not parse comparator value: BytesType(`", e.getMessage());
+            assertEquals(SyntaxException.class, e.getCause().getClass());
+        } catch (MojoFailureException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void should_give_comparator_error_message_when_key_validator_fails() { // TODO should be should_fail_when_key_validator_type_parser_class_does_not_exist
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.keyValidator("NonExistingComparator").build();
+
+        try {
+            cqlExecCassandraMojo.execute();
+        } catch (MojoExecutionException e) {
+            assertEquals("Could not parse comparator value: BytesType", e.getMessage()); // TODO should be: assertEquals("Could not parse key validator value: NonExistingComparator", e.getMessage());
+        } catch (MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_give_comparator_error_message_when_default_validator_fails() { // TODO should be should_fail_when_default_comparator_type_parser_leads_to_syntax_error
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.defaultValidator("BytesType(`").build();
+
+        try {
+            cqlExecCassandraMojo.execute();
+        } catch (MojoExecutionException e) {
+            assertEquals("Could not parse comparator value: BytesType", e.getMessage()); // TODO should be: assertEquals("Could not parse default validator value: BytesType(`", e.getMessage());
+        } catch (MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_do_nothing_with_empty_cql_statement() {
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.cqlStatement("").build();
+
+        try {
+            cqlExecCassandraMojo.execute();
+
+            assertLog(WARNING, "No CQL provided. Nothing to do.");
+        } catch (MojoExecutionException | MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_do_nothing_when_no_cql_provided() {
+        CqlExecCassandraMojo cqlExecCassandraMojo = builder.build();
+
+        try {
+            cqlExecCassandraMojo.execute();
+
+            assertLog(WARNING, "No CQL provided. Nothing to do.");
+        } catch (MojoExecutionException | MojoFailureException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    private void assertLog(Level level, String expectedContent) {
+        ArgumentCaptor<String> content = ArgumentCaptor.forClass(String.class);
+        if (level == INFO) {
+            verify(log).info(content.capture());
+        } else if (level == WARNING) {
+            verify(log).warn(content.capture());
+        }
+        assertEquals(expectedContent, content.getValue());
+    }
+
+}
