f9b31a05cd2949b5d3902533cea35c1726501c33
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2016-06-10 03:25:11 +0200

    add some tests unit tests (#18)

---
+++ src/test/java/org/codehaus/mojo/cassandra/CqlExecCassandraMojoBuilder.java
@@ -0,0 +1,57 @@
+package org.codehaus.mojo.cassandra;
+
+import org.apache.maven.plugin.logging.Log;
+
+import java.io.File;
+
+class CqlExecCassandraMojoBuilder {
+    private final CqlExecCassandraMojo cqlExecCassandraMojo;
+
+    private CqlExecCassandraMojoBuilder() {
+        this.cqlExecCassandraMojo = new CqlExecCassandraMojo();
+    }
+
+    CqlExecCassandraMojoBuilder(Log log) {
+        this();
+        this.cqlExecCassandraMojo.setLog(log);
+    }
+
+    CqlExecCassandraMojoBuilder skip() {
+        cqlExecCassandraMojo.skip = true;
+        return this;
+    }
+
+    CqlExecCassandraMojoBuilder comparator(String comparator) {
+        cqlExecCassandraMojo.comparator = comparator;
+        return this;
+    }
+
+    CqlExecCassandraMojoBuilder keyValidator(String keyValidator) {
+        cqlExecCassandraMojo.keyValidator = keyValidator;
+        return this;
+    }
+
+    CqlExecCassandraMojoBuilder defaultValidator(String defaultValidator) {
+        cqlExecCassandraMojo.defaultValidator = defaultValidator;
+        return this;
+    }
+
+    CqlExecCassandraMojoBuilder cqlScript(File cqlScript) {
+        cqlExecCassandraMojo.cqlScript = cqlScript;
+        return this;
+    }
+
+    CqlExecCassandraMojoBuilder cqlStatement(String cqlStatement) {
+        cqlExecCassandraMojo.cqlStatement = cqlStatement;
+        return this;
+    }
+
+    CqlExecCassandraMojoBuilder keyspace(String keyspace) {
+        cqlExecCassandraMojo.keyspace = keyspace;
+        return this;
+    }
+
+    CqlExecCassandraMojo build() {
+        return cqlExecCassandraMojo;
+    }
+}
