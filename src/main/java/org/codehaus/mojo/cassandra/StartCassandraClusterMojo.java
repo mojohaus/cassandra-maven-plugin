e56470ff1f60a50a98fc0904e4acdda6c00710c4
Daniel Lian <89411597+Jinn-Niam@users.noreply.github.com>
2023-11-04 18:24:19 -0400

    Remove the use of dataSet.xml, cassandraunit and thrift dependency

--- src/main/java/org/codehaus/mojo/cassandra/StartCassandraClusterMojo.java
+++ src/main/java/org/codehaus/mojo/cassandra/StartCassandraClusterMojo.java
@@ -23,9 +23,6 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.cassandraunit.DataLoader;
-import org.cassandraunit.dataset.FileDataSet;
-import org.cassandraunit.dataset.ParseException;
 
 import java.io.File;
 import java.io.IOException;
@@ -56,14 +53,6 @@ public class StartCassandraClusterMojo
      */
     @Parameter( property="cassandra.load.after.first.start", defaultValue="true")
     private boolean loadAfterFirstStart;
-
-    /**
-     * The CassandraUnit dataSet to load.
-     *
-     * @since 1.2.1-2
-     */
-    @Parameter(defaultValue="${basedir}/src/test/resources/dataSet.xml")
-    protected File cuDataSet;
 
     /**
      * Whether to ignore errors when loading the script.
@@ -176,28 +165,6 @@ public class StartCassandraClusterMojo
             if ( isClean && loadAfterFirstStart)
             {
                 execCqlFile();
-            }
-
-            if ( isClean && cuLoadAfterFirstStart && cuDataSet != null && cuDataSet.isFile() )
-            {
-                getLog().info( "Loading CassandraUnit dataSet " + cuDataSet + "..." );
-                try
-                {
-                    DataLoader dataLoader = new DataLoader( "cassandraUnitCluster", rpcAddress + ":" + rpcPort );
-                    dataLoader.load( new FileDataSet( cuDataSet.getAbsolutePath() ) );
-                }
-                catch ( ParseException e )
-                {
-                    if ( cuLoadFailureIgnore )
-                    {
-                        getLog().error( e.getMessage() + ". Ignoring as cuLoadFailureIgnore is true" );
-                    }
-                    else
-                    {
-                        throw new MojoExecutionException( "Error while loading CassandraUnit dataSet", e );
-                    }
-                }
-                getLog().info( "Finished " + cuDataSet + "." );
             }
 
             getLog().info(
--- src/main/java/org/codehaus/mojo/cassandra/StartCassandraMojo.java
+++ src/main/java/org/codehaus/mojo/cassandra/StartCassandraMojo.java
@@ -23,11 +23,7 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.cassandraunit.DataLoader;
-import org.cassandraunit.dataset.FileDataSet;
-import org.cassandraunit.dataset.ParseException;
 
-import java.io.File;
 import java.io.IOException;
 
 /**
@@ -54,14 +50,6 @@ public class StartCassandraMojo
      */
     @Parameter(property="cassandra.load.after.first.start", defaultValue="true")
     private boolean loadAfterFirstStart;
-
-    /**
-     * The CassandraUnit dataSet to load.
-     *
-     * @since 1.2.1-2
-     */
-    @Parameter(defaultValue="${basedir}/src/test/resources/dataSet.xml")
-    protected File cuDataSet;
 
     /**
      * Whether to ignore errors when loading the script.
@@ -127,28 +115,6 @@ public class StartCassandraMojo
             if ( isClean && loadAfterFirstStart)
             {
                 execCqlFile();
-            }
-
-            if ( isClean && cuLoadAfterFirstStart && cuDataSet != null && cuDataSet.isFile() )
-            {
-                getLog().info( "Loading CassandraUnit dataSet " + cuDataSet + "..." );
-                try
-                {
-                    DataLoader dataLoader = new DataLoader( "cassandraUnitCluster", rpcAddress + ":" + rpcPort );
-                    dataLoader.load( new FileDataSet( cuDataSet.getAbsolutePath() ) );
-                }
-                catch ( ParseException e )
-                {
-                    if ( cuLoadFailureIgnore )
-                    {
-                        getLog().error( e.getMessage() + ". Ignoring as cuLoadFailureIgnore is true" );
-                    }
-                    else
-                    {
-                        throw new MojoExecutionException( "Error while loading CassandraUnit dataSet", e );
-                    }
-                }
-                getLog().info( "Finished " + cuDataSet + "." );
             }
 
             getLog().info(
